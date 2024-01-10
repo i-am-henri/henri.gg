b797fec0d2a65be1d52e3283eac1cfea060d6ba0
henri <henri.me@skiff.com>
2024-01-10 11:34:35 +0100

    create a better card

---
+++ src/components/card.tsx
@@ -0,0 +1,63 @@
+
+import { useRef, useState } from "react";
+interface Props {
+    className?: string
+}
+export default function Card(props: Props, {
+    children,
+  }: {
+    children: React.ReactNode
+  }){
+    const divRef = useRef<HTMLDivElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [opacity, setOpacity] = useState(0);
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!divRef.current || isFocused) return;
+
+        const div = divRef.current;
+        const rect = div.getBoundingClientRect();
+
+        setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    };
+
+    const handleFocus = () => {
+        setIsFocused(true);
+        setOpacity(1);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+        setOpacity(0);
+    };
+
+    const handleMouseEnter = () => {
+        setOpacity(1);
+    };
+
+    const handleMouseLeave = () => {
+        setOpacity(0);
+    };
+
+    return (
+        <div
+            ref={divRef}
+            onMouseMove={handleMouseMove}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={"relative max-w-xs rounded-3xl border border-neutral-800 bg-neutral-950 p-8 " + props.className}
+        >
+            <div
+                className="pointer-events-none absolute -inset-px opacity-0 transition duration-500"
+                style={{
+                    opacity,
+                    background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.25), transparent 40%)`,
+                }}
+            />
+            {children}
+        </div>
+    );
+};
